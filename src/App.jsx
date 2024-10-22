3ab22f5ce51d049f3af09f7f0396b1ef72b09ceb
Darius Edwards <daedward@kean.edu>
2024-10-22 17:03:03 -0400

    Updated layout for professional design

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 // src/App.jsx
 import React from 'react';
-import './index.css'; // Ensure this is correct
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import './app.css'; // or './index.css' depending on which you want to use
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <main>
-        <Home />
-        <About />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <main>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 // src/components/Navbar.jsx
 import React from 'react';
-import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#resume">Resume</a></li>
+      <div className="logo">Darius Edwards</div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
