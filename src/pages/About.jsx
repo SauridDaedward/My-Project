c1f6bf5fec7c4095f51ef9b9206d654373d60314
Darius Edwards <daedward@kean.edu>
2024-10-22 21:28:52 -0400

    Updated layout for professional design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 function About() {
     return (
       <div>
-        <h1>About Me</h1>
+        <h1>Abot Me</h1>
         <p>Hi, I'm Darius Edwards. I am a senior at Kean University, working towards a degree in IT.</p>
       </div>
     );
